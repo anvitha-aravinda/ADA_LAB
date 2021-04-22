1c94978dcdbcbec508e18fae9176c8e32c818ab2
anvitha-aravinda <71319776+anvitha-aravinda@users.noreply.github.com>
2021-04-22 10:41:43 +0530

    22/04/2021

---
+++ LAB5/insertionsort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+   int i, j, count, temp, number[25];
+   printf("How many numbers u are going to enter: ");
+   scanf("%d",&count);
+   for(i=0;i<count;i++)
+    number[i] = rand() % 100 + 1;
+   printf("Elements before swapping\n");
+   for(i=0;i<count;i++)
+   printf("%d\n",number[i]);
+   for(i=0;i<count;i++){
+      for(j=i+1;j<count;j++){
+         if(number[i]>number[j]){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+   }
+
+   printf("Sorted elements: \n");
+   for(i=0;i<count;i++)
+      printf("%d\n",number[i]);
+
+   return 0;
+}
