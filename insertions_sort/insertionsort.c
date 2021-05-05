c9f5bf9d8e2543cb931fadeefe35ce19d98801fd
anvitha-aravinda <71319776+anvitha-aravinda@users.noreply.github.com>
2021-05-05 22:32:31 +0530

    Add files via upload

---
+++ insertions_sort/insertionsort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+   int i, j, count, temp;
+   printf("How many numbers u are going to enter: ");
+   scanf("%d",&count);
+   int number[count];
+   for(i=0;i<count;i++)
+    number[i] = rand() % 100 + 1;
+   printf("Elements before swapping\n");
+   for(i=0;i<count;i++)
+   printf("%d\n",number[i]);
+   for(i=1;i<count;i++){
+      temp=number[i];
+      j=i-1;
+      while((temp<number[j])&&(j>=0)){
+         number[j+1]=number[j];
+         j=j-1;
+      }
+      number[j+1]=temp;
+   }
+
+
+   printf("Sorted elements: \n");
+   for(i=0;i<count;i++)
+      printf("%d\n",number[i]);
+
+   return 0;
+}
